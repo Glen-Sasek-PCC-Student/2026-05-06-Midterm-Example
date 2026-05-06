b7f0ebb90275ea7aaa094d534c9f8e7a347e87e7
Glen-Sasek-PCC-Student <glen.sasek+student@pcc.edu>
2026-05-06 22:36:13 +0000

    not tested

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 // https://learn.zybooks.com/zybook/PCCCS161ASummer25
 // Chapter: NONE MIDTERM WEEK
 
-
 // ------------- DESIGN DOCUMENT -------------
 // A. INPUT [yes/no]:
 // B. OUTPUT [yes/no]:
@@ -27,10 +26,9 @@
 #include <limits> // NOT <climits> for cin.ignore(numeric_limits<streamsize>::max(), '\n');
 #include <iomanip>
 
-
 // RUN AND TEST COMMAND
 // cd _midterm // Or the directory containing these files
-// g++ main.cpp && for t in *.txt; do ./a.out < $t; done; rm ./a.out 
+// g++ main.cpp && for t in *.txt; do ./a.out < $t; done; rm ./a.out
 
 // MINIMAL COMPILE AND RUN, one line each
 // COMPILE
@@ -42,61 +40,149 @@
 // REMOVE ./a.out
 // rm ./a.out
 
-
-
+// COMPILE and Run Tests
+// g++ main.cpp && for t in test*txt; do echo -e "\nTEST: $t"; ./a.out < $t; done; rm ./a.out
 using namespace std;
 
-int main() {
-  string p1_name = "";
-  string p2_name = "";
-  string p3_name = "";
+int main()
+{
+  string player_A_name = "";
+  string player_B_name = "";
+  string player_C_name = "";
 
-  int p1_count = 0;
-  int p2_count = 0;
-  int p3_count = 0;
+  int player_A_count = 0;
+  int player_B_count = 0;
+  int player_C_count = 0;
 
   cout << "Welcome to the Rock Collector Championships!" << endl;
-  
+
   cout << "Enter player 1 name: ";
-  getline(cin, p1_name);
-  cout << "How many rocks did " << p1_name << " collect? ";
-  cin >> p1_count;
+  getline(cin, player_A_name);
+  cout << "How many rocks did " << player_A_name << " collect? ";
+  cin >> player_A_count;
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
   cout << "Enter player 2 name: ";
-  getline(cin, p2_name);
-  cout << "How many rocks did " << p2_name << " collect? ";
-  cin >> p2_count;
+  getline(cin, player_B_name);
+  cout << "How many rocks did " << player_B_name << " collect? ";
+  cin >> player_B_count;
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
   cout << "Enter player 3 name: ";
-  getline(cin, p3_name);
-  cout << "How many rocks did " << p3_name << " collect? ";
-  cin >> p3_count;
+  getline(cin, player_C_name);
+  cout << "How many rocks did " << player_C_name << " collect? ";
+  cin >> player_C_count;
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
   // DEBUG
   // cout << endl;
   // cout << "NAME" <<  '\t' << "COUNT" << endl;
-  // cout << p1_name << '\t' << p1_count << endl;
-  // cout << p2_name << '\t' << p2_count << endl;
-  // cout << p3_name << '\t' << p3_count << endl;
+  // cout << player_A_name << '\t' << player_A_count << endl;
+  // cout << player_B_name << '\t' << p2_count << endl;
+  // cout << player_C_name << '\t' << p3_count << endl;
 
-  //Calculations
+  // Calculations
 
-  float average = (p1_count + p2_count + p3_count) / 3.0;
+  float average = (player_A_count + player_B_count + player_C_count) / 3.0;
 
   cout << fixed << setprecision(2);
 
-  int a = p1_count;
-  int b = p2_count;
-  int c = p3_count;
+  int a = player_A_count;
+  int b = player_B_count;
+  int c = player_C_count;
 
-  string first_name = "";
-  string second_name = "";
-  string third_name = "";
+  string results = "";
 
   // Determine places
+  cout << endl;
+
+  // Three way for first
+  // a and b and c
+  if (player_A_count == player_B_count and player_B_count == player_C_count)
+  {
+    results = player_A_name + " and " + player_B_name + " and " + player_C_name + " are in a threee way tie for first.";
+  }
+
+  // Two way ties for first
+  // a and b
+  else if (player_A_count == player_B_count and player_B_count > player_C_count)
+  {
+    results = player_A_name + " and " + player_B_name + " are tied for first, " + player_C_name + " is second.";
+  }
+
+  // a and c
+  else if (player_A_count == player_C_count and player_C_count > player_B_count)
+  {
+    results = player_A_name + " and " + player_C_name + " are tied for first, " + player_B_name + " is second.";
+  }
+
+  // b and c
+  else if (player_B_count == player_C_count and player_C_count > player_A_count)
+  {
+    results = player_B_name + " and " + player_C_name + " are tied for first, " + player_A_name + " is second.";
+  }
+
+  // Two way ties for second
+  // a and b
+  else if (player_A_count == player_B_count and player_B_count < player_C_count)
+  {
+    results = player_C_name + " is first, " + player_A_name + " and " + player_B_name + " are tied for second.";
+  }
+
+  // a and c
+  else if (player_A_count == player_C_count and player_C_count < player_B_count)
+  {
+    results = player_B_name + " is first, " + player_A_name + " and " + player_C_name + " are tied for second.";
+  }
+
+  // b and c
+  else if (player_B_count == player_C_count and player_C_count < player_A_count)
+  {
+    results = player_A_name + " is first, " + player_B_name + " and " + player_C_name + " are tied for second.";
+  }
+
+  // unique count results
+  // FIRST SECOND THIRD
+  // a     b      c
+  else if (player_A_count > player_B_count and player_B_count > player_C_count)
+  {
+    results = player_A_name + " is first, " + player_B_name + " is second, " + player_C_name + " is third.";
+  }  
+
+  // a     c      b
+  else if (player_A_count > player_C_count and player_C_count > player_B_count)
+  {
+    results = player_A_name + " is first, " + player_C_name + " is second, " + player_B_name + " is third.";
+  } 
+
+  // b     a      c
+  else if (player_B_count > player_A_count and player_A_count > player_C_count)
+  {
+    results = player_B_name + " is first, " + player_A_name + " is second, " + player_C_name + " is third.";
+  } 
+
+  // b     c      a
+  else if (player_B_count > player_C_count and player_C_count > player_A_count)
+  {
+    results = player_B_name + " is first, " + player_C_name + " is second, " + player_A_name + " is third.";
+  }
+
+  // c     a      b
+  else if(player_C_count > player_A_count and player_A_count > player_B_count)
+  {
+    results = player_C_name + " is first, " + player_A_name + " is second, " + player_B_name + " is third.";
+  }
+  
+  // c     b      a
+  else if(player_C_count > player_B_count and player_B_count > player_A_count)
+  {
+    results = player_C_name + " is first, " + player_B_name + " is second, " + player_A_name + " is third.";
+  }
+
+  cout << results << endl;
+
+  cout << "The average number of rocks collected by the top three players is " << average << " rocks!" << endl;
+
   cout << "Congratulations Rock Collectors!" << endl;
 
   return 0;
